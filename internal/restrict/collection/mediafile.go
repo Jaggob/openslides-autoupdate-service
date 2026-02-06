704f3f449ac9c775222c95bba29a814b02105d4a
Jacob Bühler <jacob@MacBook-Air-von-Jacob.fritz.box>
2026-02-06 20:49:09 +0100

    feat(autoupdate): allow access to profile images

--- internal/restrict/collection/mediafile.go
+++ internal/restrict/collection/mediafile.go
@@ -120,6 +120,21 @@ func (m Mediafile) see(ctx context.Context, ds *dsfetch.Fetch, mediafileIDs ...i
 				return true, nil
 			}
 
+			// Allow profile images if the request user can see the linked user.
+			profileImageUserIDs, err := ds.Mediafile_ProfileImageUserIDs(mediafileID).Value(ctx)
+			if err != nil {
+				return false, fmt.Errorf("getting profile image user ids: %w", err)
+			}
+
+			canSeeUser, err := Collection(ctx, User{}.Name()).Modes("A")(ctx, ds, profileImageUserIDs...)
+			if err != nil {
+				return false, fmt.Errorf("can see user of profile image %d: %w", mediafileID, err)
+			}
+
+			if len(canSeeUser) >= 1 {
+				return true, nil
+			}
+
 			return false, nil
 		})
 	})
